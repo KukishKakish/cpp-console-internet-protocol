0901d8ae2c77780ab7db65f3e5f1032fee18bf5b
kukish <notgon36@gmail.com>
2024-01-01 18:55:45 +0300

    Добавление структуры для хранения данных

---
+++ main-project/internet_protocol.h
@@ -0,0 +1,25 @@
+#ifndef INTERNET_PROTOCOL
+#define INTERNET_PROTOCOL
+#include "constants.h"
+struct time {
+	int hours;
+	int minutes;
+	int seconds
+};
+struct path {
+	char disk [MAX_STRING_SIZE];
+	char users [MAX_STRING_SIZE];
+	char user1 [MAX_STRING_SIZE];
+	char programm [MAX_STRING_SIZE];
+};
+struct bytes {
+	unsigned int number;
+};
+struct iternet_protocol {
+	time start;
+	time finish;
+	bytes sent;
+	bytes recieved;
+	path program;
+};
+#endif
