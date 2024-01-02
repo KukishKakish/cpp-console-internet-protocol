5d195b7c21e9dff7ef2c7a8b09d05e6f06d6745f
kukish <notgon36@gmail.com>
2024-01-02 15:21:27 +0300

    Добавлены текстовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #5. Internet Protocol\n";
     cout << "Author: Krapiventsew D.\n";
+    cout << "Group: PInj1_d\n";
     return 0;
 }
