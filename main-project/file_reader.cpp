3cf2a0b204330b05f500e70baad62882fb13549d
kukish <notgon36@gmail.com>
2024-01-02 13:35:27 +0300

    Реализовано чтение данных из файла

--- main-project/file_reader.cpp
+++ main-project/file_reader.cpp
@@ -16,14 +16,21 @@ timer convert(char* str) {
 }
 path converts(const std::string& str) {
 	path result;
-	const std::string delimiter = "/";
+	const std::string delimiter = "\\";
 	size_t pos_disk = str.find(delimiter);
 	size_t pos_users = str.find(delimiter, pos_disk + 1);
 	size_t pos_username = str.find(delimiter, pos_users + 1);
-	result.disk = str.substr(0, pos_disk);
-	result.users = str.substr(pos_disk + 1, pos_users - pos_disk - 1);
-	result.user1 = str.substr(pos_users + 1, pos_username - pos_users - 1);
-	result.programm = str.substr(pos_username + 1);
+	size_t pos_program = str.find(delimiter, pos_username + 1);
+
+	strncpy(result.disk, str.substr(0, pos_disk).c_str(), MAX_STRING_SIZE - 1);
+	strncpy(result.users, str.substr(pos_disk + 1, pos_users - pos_disk - 1).c_str(), MAX_STRING_SIZE - 1);
+	strncpy(result.user1, str.substr(pos_users + 1, pos_username - pos_users - 1).c_str(), MAX_STRING_SIZE - 1);
+	strncpy(result.programm, str.substr(pos_username + 1, pos_program - pos_username - 1).c_str(), MAX_STRING_SIZE - 1);
+
+	result.disk[MAX_STRING_SIZE - 1] = '\0';
+	result.users[MAX_STRING_SIZE - 1] = '\0';
+	result.user1[MAX_STRING_SIZE - 1] = '\0';
+	result.programm[MAX_STRING_SIZE - 1] = '\0';
 
 	return result;
 };
@@ -43,12 +50,13 @@ void read(const char* file_name, internet_usage* array[], int& size) {
 			item->start = convert(tmp_buffer);
 			file >> tmp_buffer;
 			item->finish = convert(tmp_buffer);
+			file >> item->sent.number;
+			file >> item->recieved.number;
 			array[size++] = item;
 		}
 		file.close();
 	}
 		else{
 		throw "Ошибка открытия файла";
-	}
-
 }
+
--- main-project/main.cpp
+++ main-project/main.cpp
@@ -2,10 +2,46 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Laboratory work #9. GIT\n";
-    cout << "Variant #5. Internet Protocol\n";
-    cout << "Author: Krapiventsew D.\n";
-    return 0;
-}
+#include "internet_usage.h"
+#include "file_reader.h"
+#include "constants.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "Лабораторная работа №9. GIT\n";
+    cout << "Вариант №5. Интернет протокол\n";
+    cout << "Автор: Крапивенцев Д.\n\n";
+    internet_usage* usage[MAX_FILE_ROWS_COUNT];
+    int size;
+    try {
+        read("data.txt", usage, size);
+        for (int i = 0; i < size; i++) {
+
+            cout << usage[i]->finish.hours << ' ';
+            cout << usage[i]->finish.minutes << ' ';
+            cout << usage[i]->finish.seconds << '\n';
+            cout << usage[i]->start.hours << ' ';
+            cout << usage[i]->start.minutes << ' ';
+            cout << usage[i]->start.seconds << '\n';
+
+            cout << usage[i]->recieved.number << '\n';
+            cout << usage[i]->sent.number << '\n';
+
+            cout << usage[i]->program.disk << '\n';
+            cout << usage[i]->program.users << '\n';
+            cout << usage[i]->program.user1 << '\n';
+            cout << usage[i]->program.programm << '\n';
+            cout << '\n';
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            delete usage[i];
+        }
+    }
+        catch (const char* error)
+        {
+            cout << error << '\n';
+        }
+        return 0;
+    }
